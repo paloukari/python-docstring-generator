7e6ecefd9f89ef4d74d9013b3a29b337b22b34cf
root <root@v100a.spyros.com>
2019-09-28 16:57:19 +0000

    IBM setup

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# Expose the port to ssh directly to the container
+ufw allow 32001/tcp
+# Mount the external disk
+mkdir -m 777 /data
+mkfs.ext4 /dev/xvdc
+echo '/dev/xvdc /data                   ext4    defaults,noatime        0 0' >> /etc/fstab
+mount /data
+
+# Pull the code
+cd /data
+git clone https://github.com/paloukari/python-docstring-generator
+cd /data/python-docstring-generator/
+
+# Build the container
+docker build -t dev -f Dockerfile.dev .
+
+# Run the container
+docker run --rm --runtime=nvidia --name dev -ti --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v ~/python-docstring-generator:/src -p 32001:22 dev
