782bda6951ad0d96e8e4ab22aec62f80fe89da70
paloukari <paloukari@hotmail.com>
2019-09-30 03:26:53 +0000

    parallel graph generation

--- src/generate-graphs.py
+++ src/generate-graphs.py
@@ -30,7 +30,7 @@ def docstring_tokenize(docstr: str):
     return [t for t in docstring_regex_tokenizer.findall(docstr) if t is not None and len(t) > 0]
 
 
-def process_body(idx, total, body, docstring, data, docs_words):
+def process_body(idx, total, body, docstring):
     errors = 0
     doc_tokenizer = docstring_tokenize
     try:
@@ -45,24 +45,25 @@ def process_body(idx, total, body, docstring, data, docs_words):
                      for (origin, destination), edges
                      in visitor.graph.items() for t in edges]
 
-        docs_words.append(doc_tokenizer(docstring))
+        docs_words = doc_tokenizer(docstring)
 
         graph_node_labels = [label.strip() for (
             _, label) in sorted(visitor.node_label.items())]
 
-        data.append({"edges": edge_list,
-                     "backbone_sequence": visitor.terminal_path,
-                     "node_labels": graph_node_labels})
+        return {"edges": edge_list,
+                "backbone_sequence": visitor.terminal_path,
+                "node_labels": graph_node_labels,
+                "docs_words": docs_words}
 
     except Exception as e:
         errors += 1
 
-    return errors
+    return None
 
 
 def process_data(inputs, outputs, singlethreaded):
-    data, graph_node_labels, docs_words = [], [], []
-    
+    data = []
+
     num_inits = 0
     errors = 0
 
@@ -70,18 +71,20 @@ def process_data(inputs, outputs, singlethreaded):
 
     if singlethreaded:
         for idx, (body, docstring) in enumerate(zip(inputs, outputs)):
-            errors += process_body(idx, total, body,
-                                   docstring, data, docs_words)
+            data.append(process_body(idx, total, body,
+                                     docstring))
     else:
         num_cores = multiprocessing.cpu_count()
-        _errors = Parallel(n_jobs=2*num_cores)(
-            delayed(process_body)(idx, total, body, docstring, data, docs_words) for idx, (body, docstring) in enumerate(zip(inputs, outputs)))
-        errors = sum(_errors)
+        data = Parallel(n_jobs=2*num_cores)(
+            delayed(process_body)(idx, total, body, docstring) for idx, (body, docstring) in enumerate(zip(inputs, outputs)))
 
-    print("Generated %d graphs and had %d errors out of %d snippets" %
-          (len(data), errors, len(inputs)))
+    
+    data = [x for x in data if x is not None]
 
-    return data, docs_words
+    print("Generated %d graphs out of %d snippets" %
+          (len(data), len(inputs)))
+
+    return data
 
 
 @click.command()
@@ -110,10 +113,10 @@ def main(input, output, singlethreaded):
 
     assert len(docstring) == len(code)
 
-    graphs, docs = process_data(code, docstring, singlethreaded)
+    data = process_data(code, docstring, singlethreaded)
 
     with open(output, 'wb') as output_file:
-        pickle.dump(zip(graphs, docs), output_file)
+        pickle.dump(data, output_file)
 
 
 if __name__ == "__main__":
